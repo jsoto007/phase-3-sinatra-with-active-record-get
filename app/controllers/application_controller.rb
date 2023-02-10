7304c138cbe6255d96c92d11858ee06c28af432b
Jose Soto <jsoto7087@gmail.com>
2023-02-09 21:01:27 -0500

    complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
   get '/' do
     { message: "Hello world" }.to_json
   end
+
+  get '/games' do
+    game = Game.all.order(:title)
+    game.to_json(include: {reviews: { include: :user}})
+  end 
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    game.to_json
+  end 
 
 end
